9c1d6f1902c0302cdf4ce38dac80f216ca0235ed
Mshradha <99673738+Mshradha@users.noreply.github.com>
2022-09-08 22:31:42 +0530

    a

---
+++ index.js
@@ -0,0 +1,48 @@
+//local vs global scope
+
+
+// var a = 10; //global scope
+// function test(){
+//     console.log(a)
+// }
+// test()
+// console.log(a)
+// function test(){
+//     var a = 10 //a variable
+//         console.log(a)
+//     } //deleted
+//     test()
+// console.log(a)
+
+
+
+//var const let
+// const a = 10;
+// a = 20
+// const a = 30;
+// console.log(a)
+// let a 
+// var b
+// const c = [1,2,3]
+// c[0] = 4;
+
+
+
+//function types
+
+
+//1.function expression
+// function hello(){
+//     console.log("hi")
+// }//normal function 
+
+// const he = ()=>{
+//     console.log("Hello")
+// }//expression 
+
+// hello()
+// he()
+//2.function statement
+
+//3.anonymous function
+//4.Named functions
